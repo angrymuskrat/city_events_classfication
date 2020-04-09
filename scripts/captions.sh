0c745dde459246ebcac333d49edf9f6466ef8655
Mikhail <mlhakov2011@gmail.com>
2020-04-09 13:54:04 +0300

    add filter_posts

---
+++ scripts/captions.sh
@@ -0,0 +1,5 @@
+sudo -u postgres psql -d spb -c "\copy (select id, caption from posts where timestamp < 1483228800) to 'posts2016.csv' csv header;"
+sudo -u postgres psql -d spb -c "\copy (select id, caption from posts where timestamp between 1483228800 and 1514764800) to 'posts2017.csv' csv header;"
+sudo -u postgres psql -d spb -c "\copy (select id, caption from posts where timestamp between 1514764800 and 1546300800) to 'posts2018.csv' csv header;"
+sudo -u postgres psql -d spb -c "\copy (select id, caption from posts where timestamp between 1546300800 and 1577836800) to 'posts2019.csv' csv header;"
+sudo -u postgres psql -d spb -c "\copy (select id, caption from posts where timestamp > 1577836800) to 'posts2020.csv' csv header;"
