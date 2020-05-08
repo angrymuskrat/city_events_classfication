afc67bd1331b20b52fdaf4d25ec5b454548e6948
Mikhail <mlhakov2011@gmail.com>
2020-05-08 18:47:37 +0300

    code dump 08.05

--- scripts/captions.sh
+++ scripts/captions.sh
@@ -1,5 +1,7 @@
-sudo -u postgres psql -d spb -c "\copy (select id, caption from posts where timestamp < 1483228800) to 'posts2016.csv' csv header;"
-sudo -u postgres psql -d spb -c "\copy (select id, caption from posts where timestamp between 1483228800 and 1514764800) to 'posts2017.csv' csv header;"
-sudo -u postgres psql -d spb -c "\copy (select id, caption from posts where timestamp between 1514764800 and 1546300800) to 'posts2018.csv' csv header;"
-sudo -u postgres psql -d spb -c "\copy (select id, caption from posts where timestamp between 1546300800 and 1577836800) to 'posts2019.csv' csv header;"
-sudo -u postgres psql -d spb -c "\copy (select id, caption from posts where timestamp > 1577836800) to 'posts2020.csv' csv header;"
+#!/bin/bash
+
+sudo -u postgres psql -d $1 -c "\copy (select id, caption from posts where timestamp < 1483228800) to '$1_posts2016.csv' csv header;"
+#sudo -u postgres psql -d $1 -c "\copy (select id, caption from posts where timestamp between 1483228800 and 1514764800) to '$1_posts2017.csv' csv header;"
+#sudo -u postgres psql -d $1 -c "\copy (select id, caption from posts where timestamp between 1514764800 and 1546300800) to '$1_posts2018.csv' csv header;"
+#sudo -u postgres psql -d $1 -c "\copy (select id, caption from posts where timestamp between 1546300800 and 1577836800) to '$1_posts2019.csv' csv header;"
+#sudo -u postgres psql -d $1 -c "\copy (select id, caption from posts where timestamp > 1577836800) to '$1_posts2020.csv' csv header;"
